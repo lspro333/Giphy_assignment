c228d86355ba8a9c39d6c87e21722a5b03051c5e
Lisa Sprowls <lisasprowls@Lisas-MacBook-Pro.local>
2018-03-18 21:11:45 -0600

    updated files

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,83 @@
+$( document ).ready(function() {
+    // Array of dogBreeds\
+    var dogBreeds = ["German Shepherd", "Golden Retriever", "Poodle", "Cocker Spaniel", "Dachschund", "Bulldog", "Chihuahua", "Bloodhound", "Doberman"];
+    // Creating Functions & Methods
+    // Function that displays all gif buttons
+    function displayGifButtons(){
+        $("#gifButtonsView").empty(); 
+        for (var i = 0; i < dogBreeds.length; i++){
+            var gifButton = $("<button>");
+            gifButton.addClass("dogBreeds");
+            gifButton.addClass("btn btn-primary")
+            gifButton.attr("data-name", dogBreeds[i]);
+            gifButton.text(dogBreeds[i]);
+            $("#gifButtonsView").append(gifButton);
+        }
+    }
+    // Function to add a new dogBreeds button (Not working at the moment - need to debug)
+    function addNewButton(){
+        $("#addGif").on("click", function(){
+        var dogBreeds = $("#dogBreeds-input").val().trim();
+        if (dogBreeds == ""){
+        // User can't add a blank button
+          return false; 
+        }
+        dogBreeds.push(dogBreeds);
+    
+        displayGifButtons();
+        return false;
+        });
+    
+    }
+    // Function that displays gifs
+    function displayGifs(){
+        var dogBreeds = $(this).attr("data-name");
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + dogBreeds + "&api_key=npGIJae1BijOgso31jp5mnpK1QExRArT";
+        console.log(queryURL); 
+        $.ajax({
+            url: queryURL,
+            method: 'GET'
+        })
+        .done(function(response) {
+            console.log(response); 
+            // Erases anything from previous click
+            $("#gifsView").empty(); 
+            //Shows results of gifs
+            var results = response.data; 
+        
+            for (var i=0; i<results.length; i++){
+    
+                var gifDiv = $("<div>"); 
+                gifDiv.addClass("gifDiv");
+                // Rating of gif
+                var gifRating = $("<p>").text("Rating: " + results[i].rating);
+                gifDiv.append(gifRating);
+                // Pulling gif
+                var gifImage = $("<img>");
+                gifImage.attr("src", results[i].images.fixed_height_small_still.url); 
+                gifImage.attr("data-still",results[i].images.fixed_height_small_still.url); 
+                gifImage.attr("data-animate",results[i].images.fixed_height_small.url); 
+                gifImage.attr("data-state", "still"); 
+                gifImage.addClass("image");
+                gifDiv.append(gifImage);
+           
+                $("#gifsView").prepend(gifDiv);
+            }
+        });
+    }
+
+    displayGifButtons(); 
+    addNewButton();
+
+    $(document).on("click", ".dogBreeds", displayGifs);
+    $(document).on("click", ".image", function(){
+        var state = $(this).attr('data-state');
+        if ( state == 'still'){
+            $(this).attr('src', $(this).data('animate'));
+            $(this).attr('data-state', 'animate');
+        }else{
+            $(this).attr('src', $(this).data('still'));
+            $(this).attr('data-state', 'still');
+        }
+    });
+    });
